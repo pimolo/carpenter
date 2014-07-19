b5198e5f83cf3bc6f222cc2ca3b521ce19c03fe1
Léo-Paul COUTURIER <leopaul3@gmail.com>
2014-07-19 18:04:40 +0200

    Separate formatting and processing of user input

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
- // UTILITIES
+
+// UTILITIES
 var fs = require('fs'),
 	inquirer = require('inquirer'),
-	chalk = require('chalk');
+	chalk = require('chalk'),
+	util = require('util');
 
-console.log(chalk.white('Welcome to ' + chalk.bold('mysandbox.js') + ', your favorite tool to quickly test your little projects !'));
+console.log('Welcome to ' + chalk.bold('mysandbox.js') + ', your favorite tool to quickly test your little projects !');
 
 inquirer.prompt([{
 	type: 'input',
 	name: 'files',
+	filter: function (input) {
+		return String(input).replace(/ /g, '').split(',').filter(function (elem) {
+			return typeof elem === 'string' && elem !== '';
+		});
+	},
 	message: 'Enter source files to create, separated by commas'
 }], function (answers) {
-	var files = answers.files.replace(' ', '').split(',');
-	console.log(chalk.cyan('%s'), files);
+	console.log(chalk.cyan(util.inspect(answers.files)));
 });
