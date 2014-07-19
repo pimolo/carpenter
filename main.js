166e4cb23a856d8090ac214cce4f6c8385789690
Léo-Paul COUTURIER <leopaul3@gmail.com>
2014-07-19 10:26:40 +0200

    Setup the package installation as a binary

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-// UTILITIES
+#!/usr/bin/env node
+ // UTILITIES
 var fs = require('fs');
 
 // console.log('Welcome to mysandbox.js, your favorite tool to test fastly your little projects !')
 console.log('Begin enter source files to create :')
 
-var files = [], userIndex = 1;
+var files = [],
+	userIndex = 1;
 process.stdin.setEncoding('utf8');
 process.stdout.write(userIndex + '° ' + 'filename : ')
-process.stdin.on('readable', function() {
-  var fileName = process.stdin.read();
-  if (fileName !== null && fileName !== '' && fileName !== 'stop\n') {
-    // process.stdout.write(typeof fileName)
-    fileName = fileName.replace(/(\n)/,'');
-    files.push(fileName);
-    userIndex++;
-    process.stdout.write(userIndex + '° ' + 'filename : ')
-  }
-  if (fileName == 'stop\n') {
-    process.stdin.pause();
-    var namesToWrite = files.join('\n');
-    console.log('your files :\n' + namesToWrite);
-    console.log(files);
-  }
+process.stdin.on('readable', function () {
+	var fileName = process.stdin.read();
+	if (fileName !== null && fileName !== '' && fileName !== 'stop\n') {
+		// process.stdout.write(typeof fileName)
+		fileName = fileName.replace(/(\n)/, '');
+		files.push(fileName);
+		userIndex++;
+		process.stdout.write(userIndex + '° ' + 'filename : ')
+	}
+	if (fileName == 'stop\n') {
+		process.stdin.pause();
+		var namesToWrite = files.join('\n');
+		console.log('your files :\n' + namesToWrite);
+		console.log(files);
+	}
 });
